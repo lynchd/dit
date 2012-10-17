2ac504aa2fa10bad880ad278ca2a2a586ab75a9f
David Lynch <david.lynch@raglansoftware.com>
2012-10-17 13:20:58 +0100

    Added further resources on 7

---
+++ lectures/listings/bank/SynchATMWithdrawalFacility.java
@@ -0,0 +1,19 @@
+package ie.dit.bankexample;
+
+public class ATMWithdrawalFacility extends BankFacility
+{
+	@Override
+	public void run() {
+		{
+			super.run();
+			
+			Account davesAccount = accounts.get(1);
+			/** Processing Dave's ATM withdrawals from yesterday */
+			synchronized(davesAccount) {
+				davesAccount.withdraw(10);
+				davesAccount.withdraw(20);
+				davesAccount.withdraw(30);
+			}
+		}	
+	}
+}
